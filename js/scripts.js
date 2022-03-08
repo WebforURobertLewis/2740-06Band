fdb47333fb45871b34513c94f6c8b696d89ee084
Robert Lewis <webforURobertLewis@gmail.com>
2022-03-07 20:07:13 -0700

    Done

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function openMenu() {
+    document.getElementById("navMenu").classList.toggle("classOpen");
+    document.getElementById("burger").classList.toggle("classOpen");
+}
+
+const x = document.getElementById('burger');
+x.onclick = openMenu
+const container = document.getElementById("burger")
+container.onclick = openMenu;
